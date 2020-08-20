bc9ac6bbfde797cbfbd6e8062e37151475fcecc6
jianping.song <jianping.song@shopee.com>
2020-08-20 17:54:15 +0800

    移动端自适应方案2

--- app/template.js
+++ app/template.js
@@ -8,24 +8,12 @@ const index = (req, res) => {
     <meta name="description" content="">
     <script>
     (function flexible (window, document) {
-      var docEl = document.documentElement
-      var dpr = window.devicePixelRatio || 1
-    
-      // adjust body font size
-      function setBodyFontSize () {
-        if (document.body) {
-          document.body.style.fontSize = (12 * dpr) + 'px'
-        }
-        else {
-          document.addEventListener('DOMContentLoaded', setBodyFontSize)
-        }
-      }
-      setBodyFontSize();
-    
-      // set 1rem = viewWidth / 10
       function setRemUnit () {
-        var rem = (docEl.clientWidth > 750 ? 750 : docEl.clientWidth) / 10
-        docEl.style.fontSize = rem + 'px'
+        var html = document.getElementsByTagName('html')[0]; 
+        //屏幕的宽度（兼容处理）
+        var w = document.documentElement.clientWidth || document.body.clientWidth;
+        //750这个数字是根据你的设计图的实际大小来的，所以值具体根据设计图的大小
+        html.style.fontSize = (w > 750 ? 750 : w)  / 750 + "px";
       }
     
       setRemUnit()
@@ -37,19 +25,6 @@ const index = (req, res) => {
           setRemUnit()
         }
       })
-    
-      // detect 0.5px supports
-      if (dpr >= 2) {
-        var fakeBody = document.createElement('body')
-        var testElement = document.createElement('div')
-        testElement.style.border = '.5px solid transparent'
-        fakeBody.appendChild(testElement)
-        docEl.appendChild(fakeBody)
-        if (testElement.offsetHeight === 1) {
-          docEl.classList.add('hairlines')
-        }
-        docEl.removeChild(fakeBody)
-      }
     }(window, document))
     </script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
