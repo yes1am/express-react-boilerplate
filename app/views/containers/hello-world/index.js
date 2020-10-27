c0a0bbfe0e0fdfb49c973d9b050a0c1d1bd8004c
jianping.song <jianping.song@shopee.com>
2020-10-27 11:48:06 +0800

    feat: 新增 input 相关示例

--- app/views/containers/hello-world/index.js
+++ app/views/containers/hello-world/index.js
@@ -1,25 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 // import { Icon } from 'antd'
 import './style.less'
 import './style.css'
 
-class HelloWorld extends Component {
-  componentDidMount () {
-    this.hello()
-  }
-  hello = () => {
-    console.log(123)
-  };
-  render = () => {
-    return (
-      <React.Fragment>
-        <div className='title'>
+function HelloWorld () {
+  const [value, setValue] = useState('')
+  return (
+    <React.Fragment>
+      <div className='title'>
             Hello, express-react-dev-template
-        </div>
-        {/* <Icon type='android' /> */}
-      </React.Fragment>
-    )
-  }
+      </div>
+      <input
+        type='text'
+        // value={value}
+        onInput={(e) => {
+          // console.log('input', e.nativeEvent.inputType)
+          // if (e.nativeEvent.inputType === 'deleteCompositionText') {
+          //   console.log('input1', e.target.value)
+          // }
+          // console.log('设置 value', e.target.value)
+          console.log('input')
+          // setValue(e.target.value)
+        }}
+        // onCompositionStart={e => {
+        //   console.log('Start')
+        // }}
+        // onCompositionEnd={() => {
+        //   console.log('End')
+        // }}
+        // onCompositionUpdate={() => {
+        //   console.log('Update')
+        // }}
+        onChange={(e) => {
+          // if (e.nativeEvent.inputType === 'deleteCompositionText') {
+          //   console.log('change', e.nativeEvent.inputType)
+          // }
+          console.log('change')
+        }} />
+      {/* <Icon type='android' /> */}
+    </React.Fragment>
+  )
 }
 
 export default HelloWorld
